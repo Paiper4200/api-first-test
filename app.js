2b37ef50390a3f97e19e8a134bfe5a4fd9b99b7c
steven.stepanov <stevenstepanov25@gmail.com>
2020-02-14 11:46:43 +0200

    tiitel ja description

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const request = new XMLHttpRequest()
+
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+
 request.onload = function(){
     let data = JSON.parse(this.response)
+
+    if (request.status >= 200 && request.status < 400){
+        const container = document.getElementById('container')
+
+        data.forEach(movie =>{
+            console.log(movie.description)
+            const movie_div = document.createElement('div')
+            movie_div.innerHTML = '<div class="title">' + movie.title + '</div>' + '<div class="description">' + movie.description + '</div>' 
+            container.append(movie_div)
+        });
+    }
+
 }
+request.send()
